1f795385fffef07a624c56c4f5e26471cc9961d9
Sagor Majomder <sagormajomder@gmail.com>
2023-02-02 18:13:52 +0600

    home page complete without responsive

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// ALL Element
-const header = document.querySelector(".header");
-const btnMobNav = document.querySelector(".btn-mobile-nav");
-
-// Mobile Navigation Menu
-btnMobNav.addEventListener("click", function () {
-   header.classList.toggle("nav-open");
-});
